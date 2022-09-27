61e59147d5345f445458688ff8ba2aff485c1c78
Lupit0t <paul.piauger@ynov.com>
2022-09-27 13:53:58 +0200

    debut display pour affichage

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[][] grid = new int[][]{ { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        Display.DisplayGrid(grid);
     }
 }
