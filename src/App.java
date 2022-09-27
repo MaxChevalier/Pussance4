d7bd3067aedb40108f20247f49c200798b8fc259
Mathis <mathis.augereau@ynov.com>
2022-09-27 14:09:52 +0200

    envoie

--- src/App.java
+++ src/App.java
@@ -2,4 +2,22 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+    public class grille {
+        int[][] grille = new int[6][7];
+        public grille() {
+            for (int i = 0; i < 6; i++) {
+                for (int j = 0; j < 7; j++) {
+                    grille[i][j] = 0;
+                }
+            }
+        }
+        public void afficher() {
+            for (int i = 0; i < 6; i++) {
+                for (int j = 0; j < 7; j++) {
+                    System.out.print(grille[i][j]);
+                }
+                System.out.println();
+            }
+        }
+    }
 }
