d158e961c20748cf2889e853049b83fea41ba82d
MaxChevalier <maxime.chevalier@ynov.com>
2022-10-03 17:57:47 +0200

    Preparation etape de (TODO)

---
+++ src/Const.java
@@ -0,0 +1,5 @@
+public class Const {
+    public static final String WELCOME_MESSAGE = ""; // TODO faire le message de bienvenue
+
+    public static final String GOODBYE_MESSAGE = ""; // TODO faire le message de fin
+}
---
+++ src/GridVerif.java
@@ -0,0 +1,28 @@
+public class GridVerif {
+    public static boolean IsFinish(int[][] grid){
+        // TODO fonction qui vérifie si la grille est remplie ou si un joueur a gagné
+    }
+
+    public static int WhoWin(int[][] grid){
+        // TODO fonction qui retourne un entier pour savoir qui a gagné (return 0 si égalité)
+    }
+
+    private int[][] grid;
+
+    private GridVerif(int[][] _grid){
+        this.grid = _grid;
+    }
+
+
+    private int CheckLine(){
+        // TODO fonction qui vérifie si la ligne est gagné par un joueur et retourne l'identifiant du joueur gagnant (return 0 si aucun joueur n'a gagné)
+    }
+
+    private int CheckColumn(){
+        // TODO fonction qui vérifie si la colonne est gagné par un joueur et retourne l'identifiant du joueur gagnant (return 0 si aucun joueur n'a gagné)
+    }
+
+    private int CheckDiagonal(){
+        // TODO fonction qui vérifie si la diagonale est gagné par un joueur et retourne l'identifiant du joueur gagnant (return 0 si aucun joueur n'a gagné)
+    }
+}
