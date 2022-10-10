1c69eed2453d4c641ab5f041fd1e28877f8dab98
Lupit0t <paul.piauger@ynov.com>
2022-10-10 13:28:25 +0200

    fonction check column et check ligne

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,86 +1,86 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+// import java.io.BufferedReader;
+// import java.io.InputStreamReader;
 
-public class GameManager {
+// public class GameManager {
 
-    private Display display = new Display();
-    private int[][] grid;
+//     private Display display = new Display();
+//     private int[][] grid;
 
-    public static void main(String[] args) throws Exception {
-        System.out.println(Const.WELCOME_MESSAGE);
-        GameManager gameManager = new GameManager();
-        boolean isWantToPlay = true;
-        while (isWantToPlay){ 
-            int nbtPlayer = 0;
-            System.out.println("Combien de joueur voulez vous ? (2 ou 3)");
-            BufferedReader brInput = new BufferedReader(new InputStreamReader(System.in));
-            try {
-                nbtPlayer = Integer.parseInt(brInput.readLine());
-                try{
-                    gameManager.Game(nbtPlayer);
-                }
-                catch (Exception e){
-                    System.err.println(e.getMessage());
-                }
-            }
-            catch (Exception e){
-                System.out.println("Une Erreur est survenue\nErreur : "+ e.getMessage());
-                return;
-            }
-            System.out.println("Voulez vous rejouer ? (oui/non)");
-            switch (brInput.readLine()) {
-                case "oui":
-                    isWantToPlay = true;
-                    break;
-                case "non":
-                    isWantToPlay = false;
-                    break;
-                default:
-                    System.out.println("Entrée non reconnu\nnous considérons que vous ne voulez pas rejouer");
-                    isWantToPlay = false;
-                    break;
-            }
-        }
-        System.out.println(Const.GOODBYE_MESSAGE);
+//     public static void main(String[] args) throws Exception {
+//         System.out.println(Const.WELCOME_MESSAGE);
+//         GameManager gameManager = new GameManager();
+//         boolean isWantToPlay = true;
+//         while (isWantToPlay){ 
+//             int nbtPlayer = 0;
+//             System.out.println("Combien de joueur voulez vous ? (2 ou 3)");
+//             BufferedReader brInput = new BufferedReader(new InputStreamReader(System.in));
+//             try {
+//                 nbtPlayer = Integer.parseInt(brInput.readLine());
+//                 try{
+//                     gameManager.Game(nbtPlayer);
+//                 }
+//                 catch (Exception e){
+//                     System.err.println(e.getMessage());
+//                 }
+//             }
+//             catch (Exception e){
+//                 System.out.println("Une Erreur est survenue\nErreur : "+ e.getMessage());
+//                 return;
+//             }
+//             System.out.println("Voulez vous rejouer ? (oui/non)");
+//             switch (brInput.readLine()) {
+//                 case "oui":
+//                     isWantToPlay = true;
+//                     break;
+//                 case "non":
+//                     isWantToPlay = false;
+//                     break;
+//                 default:
+//                     System.out.println("Entrée non reconnu\nnous considérons que vous ne voulez pas rejouer");
+//                     isWantToPlay = false;
+//                     break;
+//             }
+//         }
+//         System.out.println(Const.GOODBYE_MESSAGE);
         
-    }
+//     }
 
-    private void Game(int nbtPlayer){
-        // génére la grille en fonction du nombre de joueur
-        switch (nbtPlayer) {
-            case 2:
-                grid = display.GenerateGrid(8,6);
-                break;
-            case 3:
-                grid = display.GenerateGrid(12,10);
-                break;
-            default:
-                throw new IllegalArgumentException("Unexpected value: " + nbtPlayer);
-        }
+//     private void Game(int nbtPlayer){
+//         // génére la grille en fonction du nombre de joueur
+//         switch (nbtPlayer) {
+//             case 2:
+//                 grid = display.GenerateGrid(8,6);
+//                 break;
+//             case 3:
+//                 grid = display.GenerateGrid(12,10);
+//                 break;
+//             default:
+//                 throw new IllegalArgumentException("Unexpected value: " + nbtPlayer);
+//         }
 
-        int turnPlayer = 1; // représante lejoeur qui doit jouer
+//         int turnPlayer = 1; // représante lejoeur qui doit jouer
 
-        // répétition tant que la grille n'est pas remplie ou qu'un joueur n'a pas gagné
-        while (!IsFinish())
-        {
-            UserPlay(turnPlayer);
-            turnPlayer = (turnPlayer%nbtPlayer)+1; // passage au joueur suivant
-        }
+//         // répétition tant que la grille n'est pas remplie ou qu'un joueur n'a pas gagné
+//         while (!IsFinish())
+//         {
+//             UserPlay(turnPlayer);
+//             turnPlayer = (turnPlayer%nbtPlayer)+1; // passage au joueur suivant
+//         }
 
-        int winner = WhoWin();
+//         int winner = WhoWin();
 
-        switch (winner){
-            case 0:
-                System.out.println("Match nul");
-                break;
-            default:
-                System.out.println("Le joueur n°"+winner+" a gagné");
-                break;
-        }
-    }
+//         switch (winner){
+//             case 0:
+//                 System.out.println("Match nul");
+//                 break;
+//             default:
+//                 System.out.println("Le joueur n°"+winner+" a gagné");
+//                 break;
+//         }
+//     }
 
-    private void UserPlay(int turnPlayer){
-        // TODO créer la fonction pour permettre au joueur n°turnPlayer de jouer
-    }
+//     private void UserPlay(int turnPlayer){
+//         // TODO créer la fonction pour permettre au joueur n°turnPlayer de jouer
+//     }
     
-}
+// }
--- src/GridVerif.java
+++ src/GridVerif.java
@@ -1,11 +1,12 @@
 public class GridVerif {
-    public static boolean IsFinish(int[][] grid){
-        // TODO fonction qui vérifie si la grille est remplie ou si un joueur a gagné
-    }
+    // public static boolean IsFinish(int[][] grid){
+    //     // TODO fonction qui vérifie si la grille est remplie ou si un joueur a gagné
+    // }
 
-    public static int WhoWin(int[][] grid){
-        // TODO fonction qui retourne un entier pour savoir qui a gagné (return 0 si égalité)
-    }
+    // public static int WhoWin(int[][] grid){
+    //     // TODO fonction qui retourne un entier pour savoir qui a gagné (return 0 si égalité)
+
+    // }
 
     private int[][] grid;
 
@@ -13,16 +14,63 @@ public class GridVerif {
         this.grid = _grid;
     }
 
+    public static void test(int[][] _grid){
+        GridVerif gridVerif = new GridVerif(_grid);
+        System.out.println(gridVerif.CheckLine());
+        System.out.println(gridVerif.CheckColumn());
+    }
+
+    public static void main(String[] args){
+        int[][] grid = {{1,1,1,1},{0,0,1,0},{0,0,1,0},{0,0,2,0},{0,0,1,0}};
+        GridVerif.test(grid);
+    }
 
     private int CheckLine(){
+        int[] infocheck = {0,0}; // player, nbt a la suite
         // TODO fonction qui vérifie si la ligne est gagné par un joueur et retourne l'identifiant du joueur gagnant (return 0 si aucun joueur n'a gagné)
+        for(int i = 0 ; i < this.grid.length ; i++){
+            for(int j = 0 ; j < this.grid[i].length ; j++){
+                if(this.grid[i][j] == infocheck[0]){
+                    if (infocheck[1] == 3){
+                        return infocheck[0];
+                    }else{
+                        infocheck[1]++;
+                    }
+                }
+                else{
+                    infocheck[0] = this.grid[i][j];
+                    infocheck[1] = 1;
+                }
+            }
+           
+        }
+        return 0;
+
     }
 
     private int CheckColumn(){
         // TODO fonction qui vérifie si la colonne est gagné par un joueur et retourne l'identifiant du joueur gagnant (return 0 si aucun joueur n'a gagné)
+        int[] infocheck = {0,0}; // player, nbt a la suite
+        for(int i = 0 ; i < this.grid.length ; i++){
+            for(int j = 0 ; j < this.grid[i].length ; j++){
+                if(this.grid[j][i] == infocheck[0]){
+                    if (infocheck[1] == 3){
+                        return infocheck[0];
+                    }else{
+                        infocheck[1]++;
+                    }
+                }
+                else{
+                    infocheck[0] = this.grid[j][i];
+                    infocheck[1] = 1;
+                }
+            }
+           
+        }
+        return 0;
     }
 
-    private int CheckDiagonal(){
-        // TODO fonction qui vérifie si la diagonale est gagné par un joueur et retourne l'identifiant du joueur gagnant (return 0 si aucun joueur n'a gagné)
-    }
+    // private int CheckDiagonal(){
+    //     // TODO fonction qui vérifie si la diagonale est gagné par un joueur et retourne l'identifiant du joueur gagnant (return 0 si aucun joueur n'a gagné)
+    // }
 }
